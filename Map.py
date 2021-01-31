8ad39dfef4f14893ce15745e3a167f3d649fa424
Sina Kamali <sininoir@gmail.com>
2021-01-31 23:35:14 +0330

    still in early phase

--- Map.py
+++ Map.py
@@ -1,5 +1,9 @@
 
 
 class Map:
+    MAP_SIZE = (13,13)
     def __init__(self, path_to_save=None):
-        print('figure out how to save and generate maps')
+        self.map = []
+        for i in range(MAP_SIZE[0]):
+            for j in range(MAP_SIZE[1]):
+                
