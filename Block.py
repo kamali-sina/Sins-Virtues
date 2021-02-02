9d1b2cac8652202fcd948fd92a093b4fa6034d7e
Sina Kamali <sininoir@gmail.com>
2021-02-02 21:49:01 +0330

    fixed bug in blocks

--- Block.py
+++ Block.py
@@ -5,24 +5,42 @@ from Enemy import get_random_enemy
 class Block:
     def __init__(self):
         self.name = ""
+        self.tags = []
+        self.rarity = 1
+
+    def __str__(self):
+        return self.name
 
 class CastleBlock(Block):
     def __init__(self):
+        self.rarity = 9999
         self.name = "castle"
+        self.tags = ['special']
 
 class DigableBlock(Block):
     def __init__(self):
+        self.ITEM_CHANCE = 0.4
+        self.tags = ['random', 'special']
         self.name = "digable"
-        self.item_inside = Item.get_random_item(luck_factor=0)
+        self.rarity = 10
+        self.contains_item = random() < self.ITEM_CHANCE
+        if (self.contains_item):
+            self.item_inside = Item.get_random_item(luck_factor=0)
 
 class NormalBlock(Block):
     def __init__(self):
+        self.tags = ['random']
+        self.rarity = 1
         self.name = "normal"
 
 class HomeBlock(Block):
-    ENEMY_CHANCE = 0.3
     def __init__(self):
+        self.ENEMY_CHANCE = 0.3
+        self.tags = ['random', 'special']
+        self.rarity = 70
         self.name = "home"
-        self.does_contain_enemy = random() < ENEMY_CHANCE
-        if (self.does_contain_enemy):
+        self.contains_enemy = random() < self.ENEMY_CHANCE
+        if (self.contains_enemy):
             self.enemy = get_random_enemy()
+
+ALL_BLOCKS = [CastleBlock, DigableBlock, NormalBlock, HomeBlock]
