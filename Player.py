6e471af4a2b5c373c0dbc058ea95b94d35e3bfaf
Sina Kamali <sininoir@gmail.com>
2021-02-03 22:23:03 +0330

    added update cord

--- Player.py
+++ Player.py
@@ -2,14 +2,19 @@
 STARTING_MAX_HEALTH = 10
 STARTING_GOLD = 0
 STARTING_LOCATION = [0,0]
+
 class Player:
     def __init__(self, path_to_save=None):
         self.inventory = []
         self.health = STARTING_MAX_HEALTH
         self.gold = STARTING_GOLD
         self.location = STARTING_LOCATION
-        if (path_to_save):
-            self.load_from_save(path_to_save)
+        # if (path_to_save):
+        #     self.load_from_save(path_to_save)
 
     def load_from_save(self, path_to_save):
-        print('loading from save!')
+        print('player: loading from save!')
+    
+    def move(self, tup):
+        self.location[0] += tup[0]
+        self.location[1] += tup[1]
