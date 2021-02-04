7ae5ed5a1656c5a0ee159bc482c2f4c1c8c2b042
Sina Kamali <sininoir@gmail.com>
2021-02-04 22:57:42 +0330

    added __str__ funcs

--- Item.py
+++ Item.py
@@ -1,5 +1,5 @@
 from random import random
-
+from termcolor import colored, cprint
 
 ALL_TAGS = ['utility', 'attack', 'hp']
 
@@ -8,15 +8,27 @@ class Item:
         self.name = "ite"
         self.tags = []
 
+    def __str__(self):
+        return self.name
+
 class UtilityItem(Item):
     def __init__(self):
+        self.INITIAL_USES = 0
         self.name = 'utility'
         self.tags = ['utility']
+        self.uses = self.INITIAL_USES
+    
+    def __str__(self):
+        return colored(self.name, "blue") + ' ---- uses remaining: ' + str(self.uses)
 
 class HpItem(Item):
     def __init__(self):
+        self.name = 'hp'
         self.tags = ["hp"]
         self.hp = 0
+    
+    def __str__(self):
+        return colored(self.name, "green") + ' ---- restores: ' + str(self.hp) + ' hp'
 
 class AttackItem(Item):
     def __init__(self):
@@ -25,6 +37,10 @@ class AttackItem(Item):
         self.damage = 0
         #TODO: think about this
         self.agile_mult = 1
+
+    def __str__(self):
+        return colored(self.name, "red") + ' ---- damage: ' + str(self.damage)
+
 
 class Shovel(UtilityItem):
     def __init__(self):
@@ -61,10 +77,12 @@ class Sword(AttackItem):
 class Apple(HpItem):
     def __init__(self):
         self.tags = ["hp"]
+        self.name = "apple"
         self.hp = 3
 
 class Meat(HpItem):
     def __init__(self):
+        self.name = "meat"
         self.tags = ['attack', 'hp']
         self.damage = 1
         self.hp = 6
