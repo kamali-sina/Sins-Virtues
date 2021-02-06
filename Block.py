3ef14e43a93c5e38ec39fec5298927536f661267
Sina Kamali <sininoir@gmail.com>
2021-02-06 19:49:34 +0330

    added get_info func to blocks

--- Block.py
+++ Block.py
@@ -7,6 +7,9 @@ class Block:
         self.name = ""
         self.tags = []
         self.rarity = 1
+    
+    def get_info(self):
+        return 'default block'
 
     def __str__(self):
         return self.name
@@ -16,6 +19,9 @@ class CastleBlock(Block):
         self.rarity = 9999
         self.name = "castle"
         self.tags = ['special']
+    
+    def get_info(self):
+        return 'A castle in the middle of nowhere?!'
 
 class DigableBlock(Block):
     def __init__(self):
@@ -26,6 +32,9 @@ class DigableBlock(Block):
         self.contains_item = random() < self.ITEM_CHANCE
         if (self.contains_item):
             self.item_inside = Item.get_random_item(luck_factor=0)
+    
+    def get_info(self):
+        return 'It looks like I can dig here with a shovel!'
 
 class NormalBlock(Block):
     def __init__(self):
@@ -37,6 +46,18 @@ class NormalBlock(Block):
         if (self.contains_item):
             self.item_inside = Item.get_random_item(luck_factor=0)
 
+    def get_info(self):
+        if (self.contains_item):
+            return 'Wow there is a chest here!'
+        else:
+            return 'nothing special here.'
+
+    def __str__(self):
+        me = self.name
+        if (self.contains_item):
+            me += '*'
+        return me
+
 class HomeBlock(Block):
     def __init__(self):
         self.ENEMY_CHANCE = 0.3
@@ -46,5 +67,8 @@ class HomeBlock(Block):
         self.contains_enemy = random() < self.ENEMY_CHANCE
         if (self.contains_enemy):
             self.enemy = get_random_enemy()
+
+    def get_info(self):
+        return 'This looks like a place to rest.'
 
 ALL_BLOCKS = [CastleBlock, DigableBlock, NormalBlock, HomeBlock]
