eb07644b66107591eb6bdadceb232550e0c3a691
sinku <sininoir@gmail.com>
2021-03-18 12:11:05 +0330

    added save handling to the game

--- VnS.py
+++ VnS.py
@@ -1,6 +1,6 @@
 import sys
 import os
-from ConsoleHandler import error,help_if_needed, toturial_if_needed, NEWGAME, LOADGAME, DEVMODE, info_if_needed
+from ConsoleHandler import error,help_if_needed, toturial_if_needed, NEWGAME, LOADGAME, DEVMODE, info_if_needed, get_file_path, DELIM
 from Game import Game
 from sys import exit
 
@@ -11,9 +11,12 @@ if __name__ == "__main__":
     help_if_needed(sys.argv[1])
     info_if_needed(sys.argv[1])
     toturial_if_needed(sys.argv[1])
-    path = './'
+    path = get_file_path(sys.argv[0]) + 'saves' + DELIM
     if (len(sys.argv) > 2):
         path = sys.argv[2]
+        if (not path.endswith(DELIM)):
+            path = path + DELIM
+        path = path + 'saves' + DELIM
     game_session = None
     if (sys.argv[1] == NEWGAME):
         game_session = Game(path_to_savefiles=path, newgame=True)
