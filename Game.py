e874a10bb71badf25727db89affd81f662f9bad3
sinku <sininoir@gmail.com>
2021-03-22 16:18:53 +0430

    added daynight cycle to game

--- Game.py
+++ Game.py
@@ -1,10 +1,12 @@
-from ConsoleHandler import slow, intro_cutscene, dialog, error
+from ConsoleHandler import slow, intro_cutscene, dialog, error, notification
 import ConsoleHandler
 from Block import DigableBlock, NormalBlock, ShopBlock
 import Block
 from termcolor import colored
 from Item import AttackItem
 from time import sleep
+from random import random
+from Enemy import get_random_enemy
 from sys import exit
 from Player import Player
 from Map import Map
@@ -24,6 +26,10 @@ class Game:
         self.SHOP_COMMANDS_HANDLER = [self.inventory, self.info, self.commands, self.stock, self.buy, self.sell, self.exit_shop]
         self.BLACKSMITH_COMMANDS = ['inventory', 'info', 'commands', 'upgrade', 'scrap', 'exit']
         self.BLACKSMITH_COMMANDS_HANDLER = [self.inventory, self.info, self.commands, self.upgrade, self.scrap, self.exit_shop]
+        self.world_timer = float(0)
+        self.available_times = ['morning', 'noon', 'night']
+        self.NIGHT_ENEMY_CHANCE = 0.51
+        self.time_of_day = self.available_times[0]
         self.my_time = float(0)
         self.enemy_time = float(0)
         if (newgame):
@@ -53,7 +59,6 @@ class Game:
         open_file = open(file_name, "rb")
         self.map = pickle.load(open_file)
         open_file.close()
-        
 
     def check_save_exists(self, path_to_savefiles):
         files = ['player', 'map']
@@ -67,8 +72,17 @@ class Game:
         self.state = 'normal'
         while(True):
             self.set_command_set()
+            self.update_time_of_day()
             input_str = input("> ").strip().lower()
-            self.process_input(input_str)
+            validity = self.process_input(input_str)
+            self.spawn_random_enemy_if_needed(validity)
+    
+    def spawn_random_enemy_if_needed(self, validity):
+        if (self.time_of_day == self.available_times[-1] and self.state == 'normal' and validity):
+            if (random() < self.NIGHT_ENEMY_CHANCE):
+                enemy = get_random_enemy()
+                ConsoleHandler.encountered_enemy_at_night_dialog(enemy.name)
+                self.fight_enemy(enemy)
     
     def set_command_set(self):
         if (self.state == 'normal'):
@@ -87,14 +101,36 @@ class Game:
             self.current_commandset = self.BLACKSMITH_COMMANDS
             self.current_commandset_handler = self.BLACKSMITH_COMMANDS_HANDLER
     
+    def update_world_timer(self, time):
+        Multi = 1
+        self.world_timer += time * Multi
+    
+    def reset_world_timer(self):
+        self.world_timer = float(0)
+        self.time_of_day = self.available_times[0]
+
+    def update_time_of_day(self):
+        self.world_timer = self.world_timer % 24
+        time = -1
+        if (self.world_timer < 14):
+            time = 0
+        elif (self.world_timer < 16):
+            time = 1
+        else:
+            time = 2
+        if (self.time_of_day != self.available_times[time]):
+            ConsoleHandler.new_time_dialog(time)
+        self.time_of_day = self.available_times[time]
+    
     def process_input(self, input_str):
-        if (not self.validate_input(input_str)): return
+        if (not self.validate_input(input_str)): return False
         print()
         dupped_str = input_str.split()
         command = dupped_str[0]
         index = self.current_commandset.index(command)
         self.current_commandset_handler[index](dupped_str)
         print()
+        return True
     
     def validate_input(self, input_str):
         dupped_str = input_str.split()
@@ -132,6 +168,7 @@ class Game:
             return
         self.player.move(moveset_handler[index])
         self.new_block_dialog()
+        self.update_world_timer(0.38)
         return
     
     def inventory(self, dupped_str):
@@ -139,6 +176,7 @@ class Game:
             self.unknown_command_dialog()
             return
         self.my_time += 0.5
+        self.update_world_timer(0.15)
         self.player.print_inventory()
     
     def stock(self, dupped_str):
@@ -151,6 +189,7 @@ class Game:
             print(f' {block.stock[0][i]}', end="")
             spaces = 21 - len(block.stock[block.NAME][i])
             print(f'{" " * spaces}{colored(block.stock[block.PRICE][i],"yellow")}')
+        self.update_world_timer(0.18)
     
     def buy(self, dupped_str):
         if (len(dupped_str) < 2):
@@ -169,6 +208,7 @@ class Game:
         item = block.buy_item(index)
         self.player.coin -= price
         self.player.add_item(item)
+        self.update_world_timer(0.1)       
         print(f'buyed {dupped_str[1]} for {price} coins')
     
     def sell(self, dupped_str):
@@ -180,12 +220,14 @@ class Game:
             ConsoleHandler.dont_have_items_dialog()
             return
         price = self.player.inventory[index].get_sell_price()
+        self.update_world_timer(0.11)
         ans = ConsoleHandler.prompt(f'sell item for {price} coins?')
         if (ans != 0):
             print(f'{dupped_str[1]} sold for {colored(self.player.sell(index),"yellow")}')
     
     def exit_shop(self, dupped_str):
         print(f'exiting {self.state}...')
+        self.update_world_timer(0.03)
         self.state = 'normal'
     
     def equip(self, dupped_str):
@@ -196,6 +238,7 @@ class Game:
         if (index == -1):
             ConsoleHandler.dont_have_items_dialog()
             return
+        self.update_world_timer(0.13)
         if('attack' in self.player.inventory[index].tags):
             self.my_time += 1.5
             self.player.equip_item(self.player.inventory[index])
@@ -211,6 +254,7 @@ class Game:
         if (index == -1):
             ConsoleHandler.dont_have_items_dialog()
             return
+        self.update_world_timer(0.1)        
         if('utility' in self.player.inventory[index].tags):
             self.player.inventory[index].use(self, index)
         elif ('hp' in self.player.inventory[index].tags):
@@ -218,14 +262,24 @@ class Game:
         else:
             ConsoleHandler.cant_use_item_dialog()
             return
+        self.update_world_timer(0.20)
         self.my_time += 1
     
+    def get_clock_time(self):
+        t =  (self.world_timer + 5) % 24
+        h = int(t)
+        m = int((t - h) * 60)
+        return f'{h}:{m}'
+        
+
     def info(self, dupped_str):
         if (len(dupped_str) != 1):
             self.unknown_command_dialog()
             return
+        self.update_world_timer(0.1)        
         self.player.print_info()
         block = self.map.get(self.player.location)
+        print(f'time: {self.get_clock_time()}')
         if (self.state == 'normal'): print(f'current block is {colored(block.name,"magenta")}')
         elif (self.state == 'fight'): print(f'enemy has {colored(self.enemy.hp,"red")} hp left')
 
@@ -254,6 +308,7 @@ class Game:
             return
         result = self.player.inventory[index].upgrade()
         self.player.scrap -= price
+        self.update_world_timer(0.5)        
         print(f'upgraded {dupped_str[1]} for {price} parts. {result}')
     
     def scrap(self, dupped_str):
@@ -268,13 +323,16 @@ class Game:
             ConsoleHandler.item_not_scrappable_dialog()
             return
         price = self.player.inventory[index].get_scrap_parts()
+        self.update_world_timer(0.1)       
         ans = ConsoleHandler.prompt(f'scrap {self.player.inventory[index]} for {price} scraps?')
         if (ans != 0):
             print(f'{dupped_str[1]} scrapped for {colored(self.player.scrap_item(index),"grey")} scraps.')
-    
+            self.update_world_timer(0.6)
+
     def attack(self, dupped_str):
         self.attacked = True
         self.my_time += self.enemy.speed
+        self.update_world_timer(0.05)
         self.player.attack(self.enemy)
 
     def commands(self, dupped_str):
@@ -331,9 +389,9 @@ class Game:
     
     def fight_enemy(self, enemy):
         print(colored('\n--Entered Battle--\n','red'))
-        sleep(0.7)
-        print('enemy info:\n' + str(enemy) + '\n')
-        sleep(0.6)
+        notification('enemy info:\n' + str(enemy), speed=18)
+        print()
+        save_state = self.state
         self.state = 'fight'
         self.set_command_set()
         self.enemy = enemy
@@ -343,7 +401,7 @@ class Game:
         while(True):
             if (self.enemy_time < self.my_time):
                 #Enemy's turn to attack!
-                self.enemy.attack(self.player)
+                notification(self.enemy.attack(self.player), speed=26)
                 self.enemy_time += self.player.equipped.speed
             else:
                 #our turn to attack
@@ -359,14 +417,22 @@ class Game:
                 ConsoleHandler.death_dialog()
                 exit()
             elif(self.enemy.hp <= 0):
-                print(f'the {colored(self.enemy.name, "red")} is dead.')
+                notification(f'the {colored(self.enemy.name, "red")} is dead.', speed=20)
+                print()
                 dialog("You", self.enemy.get_kill_dialog(), "yellow", speed=18)
                 self.player.coin += self.enemy.bounty
                 break
         self.player.reset_status_effects()
-        self.state = 'normal'
+        self.state = save_state
     
+    def is_block_closed(self, block_name):
+        if (self.time_of_day == 'night'): 
+            ConsoleHandler.shop_is_closed_dialog(block_name)
+            return True
+        return False
+
     def enter_shop(self):
+        if (self.is_block_closed('shop')): return
         print(colored('--Entered Shop--\n','yellow'))
         ConsoleHandler.welcome_to_shop_dialog()
         self.state = 'shop'
@@ -377,6 +443,7 @@ class Game:
         self.set_command_set()
     
     def enter_blacksmith(self):
+        if (self.is_block_closed('blacksmith')): return
         print(colored('--Entered Blacksmith--\n','grey'))
         ConsoleHandler.welcome_to_blacksmith_dialog()
         self.state = 'blacksmith'
